8be66340a43abb336e967d219d9f345528f88d05
Mogrix <petrasanton027@gmail.com>
2022-04-23 15:10:25 +0100

    made it to work

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let charSelector = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let generatedPassword = "";
-let passwordlenght = 12;
 
 function passwordGenerator(passwordlenght){
     for(let i = 0; i <= passwordlenght; i++){
-        var randomChar = Math.floor(Math.random() * charSelector.length);
-        
-        generatedPassword =+ randomChar;
+        generatedPassword += charSelector.charAt(Math.floor(Math.random() * charSelector.length));
     }
     return generatedPassword;
 }
-passwordGenerator();
+console.log(passwordGenerator(12));
